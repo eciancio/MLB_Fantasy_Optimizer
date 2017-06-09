3a7d2106623e3a0d90aa768975a59c2e39a0c469
eciancio <eciancio@llnw.com>
2017-06-09 16:24:52 -0400

    Input the day and have it predict

--- classifier.py
+++ classifier.py
@@ -5,24 +5,52 @@ import urllib
 import numpy as np
 import tempfile
 import urllib
+import mlbgame
+from operator import itemgetter
+
+day = input("Day ")
+month = input("Month ")
+today = mlbgame.day(2017, month, day - 1)
+pre_data = []
+for game in today:
+    print(game)
+    mlb_parser.get_players(game.game_id,pre_data) 
+
+#read in av data
+x_pred = []
+x_pred2 =[]
+x_pred3 = []
+for data in pre_data:
+	x_pred.append(data[2])
+	x_pred2.append(data[3])
+        x_pred3.append(data[1])
+
+
+
 train_file = tempfile.NamedTemporaryFile()
 test_file = tempfile.NamedTemporaryFile()
 
 data = mlb_parser.main()
 
-testing_data = data[:100]
-train_data = data[100:]
+testing_data = data[:300]
+train_data = data[300:]
+print len(train_data)
 
 #read in av data
 x_train = []
+x_train2 =[]
+x_train3 = []
 for data in train_data:
 	x_train.append(data[2])
-
+	x_train2.append(data[3])
+        x_train3.append(data[1])
 x_test = []
+x_test2 = []
+x_test3 = []
 for data in testing_data:
 	x_test.append(data[2])
-
-
+        x_test2.append(data[3])
+        x_test3.append(data[1])
 #read in hit data 
 y_train = []
 for data in train_data:
@@ -37,33 +65,77 @@ for data in testing_data:
 
 
 #make columns 
-COLUMNS= ['avg','hits']
+COLUMNS= ['avg','era','hits']
 test_continious_cols = []
 test_labels = []
 
 #avg column 
 avg = tf.contrib.layers.real_valued_column("avg")
+era = tf.contrib.layers.real_valued_column("era")
+pos = tf.contrib.layers.sparse_column_with_hash_bucket("pos", hash_bucket_size=1000)
 
 
 model_dir = tempfile.mkdtemp()
-m = tf.contrib.learn.LinearClassifier(feature_columns=[avg],
+m = tf.contrib.learn.LinearClassifier(feature_columns=[avg,era,pos],
           model_dir=model_dir)
 
 def input_fn_traint():
     #make cols
     continuous_cols = {'avg': tf.constant(x_train)}
+    continuous_cols['era'] = tf.constant((x_train2))
+    continuous_cols['pos'] = tf.SparseTensor(
+            indices=[[i, 0] for i in range(len(x_train3))],
+            values=x_train3,
+            dense_shape=[len(x_train3), 1])
+
     labels = tf.constant(y_train)
     return continuous_cols, labels
 
 def input_fn_test():
     test_continuous_cols = {'avg': tf.constant(x_test)}
+    test_continuous_cols['era'] = tf.constant(x_test2)
+    test_continuous_cols['pos'] =tf.SparseTensor(
+            indices=[[i, 0] for i in range(len(x_test3))],
+            values=x_test3,
+            dense_shape=[len(x_test3), 1])
+
     test_labels = tf.constant(y_test)
     return test_continuous_cols, test_labels
 
-m.fit(input_fn=input_fn_traint, steps=200)
+
+
+def input_fn_pred():
+    test_continuous_cols = {'avg': tf.constant(x_pred)}
+    test_continuous_cols['era'] = tf.constant(x_pred2)
+    test_continuous_cols['pos'] =tf.SparseTensor(
+            indices=[[i, 0] for i in range(len(x_pred3))],
+            values=x_pred3,
+            dense_shape=[len(x_pred3), 1])
+
+    test_labels = tf.constant(y_test)
+    return test_continuous_cols, test_labels
+
+
+
+
+
+m.fit(input_fn=input_fn_traint, steps=1000)
 results = m.evaluate(input_fn=input_fn_test, steps=1)
 for key in sorted(results):
         print("%s: %s" % (key, results[key]))
+predictions = m.predict_proba(input_fn=input_fn_pred)
+predict_array =[]
+for classs in predictions:
+    predict_array.append(classs)
+
+final_array =[]
+for i in range(len(pre_data)):
+    inner_array = [pre_data[i][0],predict_array[i][0],predict_array[i][1]]
+    final_array.append(inner_array)
+
+sorted_final = sorted(final_array, key=itemgetter(1))
+for i in range(10):
+    print(sorted_final[i])
 
 
 def input_fn(df):
--- mlb_parser.py
+++ mlb_parser.py
@@ -5,36 +5,44 @@ def get_batch(year,month):
 	games = mlbgame.games(year, month)
 	return games
 
-def get_batters(games):
+def get_batters(games,data):
 	for day in games:
 		for game in day: 
-			get_players(game.game_id)	
-def get_players(game_id):
+			get_players(game.game_id,data)	
+def get_players(game_id,data):
 	try:
 		stats = mlbgame.player_stats(game_id)
-		get_stats(1,stats)
+		get_stats(1,stats,data)
+                get_stats(0,stats,data)
 	except:
 		print('')
 
-def get_stats(team,stats):
+def get_stats(team,stats,data):
 	place = ""
 	if team == 1:
 		place = 'home_batting'
 		s_p = stats['away_pitching'][0]
-	
-	for player in stats[place]:
-		compile_stats(player,s_p)
+        else:
+		place = 'away_batting'
+		s_p = stats['home_pitching'][0]
 
-def compile_stats(hitter,pitcher):
-	stats = [hitter.pos, 
+	for player in stats[place]:
+		compile_stats(player,s_p,data)
+
+def compile_stats(hitter,pitcher,data):
+    if hitter.pos == 'P':
+        pass
+    else:
+        stats = [hitter.name,
+                hitter.pos, 
 		hitter.avg,
 		pitcher.era,
 		hitter.h]
-	player = { hitter.name : stats}	
-	data.append(player)
+	data.append(stats)
 
-data = []
-get_batters(get_batch(2017,5))
-
-for i in range(100):
-	print(data[i])
+def main():
+    data = []
+    get_batters(get_batch(2017,5),data)
+    get_batters(get_batch(2016,8),data)
+    get_batters(get_batch(2016,7),data)
+    return data
